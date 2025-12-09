f7cd25e5309daaef2b72007cb43c2d715d27d37a
abhijeet choudhury <abhijeetchoudhury@Abhijeets-MacBook-Air.local>
2025-12-09 22:38:46 +0530

    query client update

--- src/lib/queryClient.ts
+++ src/lib/queryClient.ts
@@ -3,8 +3,8 @@ import { QueryClient } from '@tanstack/react-query'
 export const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
-      staleTime: 5 * 60 * 1000,
-      refetchOnWindowFocus: false,
+      // staleTime: 5 * 60 * 1000,
+      refetchOnWindowFocus: true,
       retry: 1,
     },
     mutations: {
